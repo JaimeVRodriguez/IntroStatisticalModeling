9bb33201a7ffa65e4c74de66b2c8bf7e07112009
Jaime Rodriguez <jaime@Jaimes-MacBook-Pro.local>
2022-09-01 19:59:07 -0400

    Start Intro Lab 2

---
+++ Module 1/Intro/Intro Lab 2.R
@@ -0,0 +1,27 @@
+library(airports)
+
+Q1 <- rows <- NROW(usairports)
+
+Q2 <- usairports
+
+Q3 <- Q2
+
+Q4 <- c(1, 2, 3)
+
+Q5 <- c("Larry Fine", "Curly Howard", "Moe Howard")
+
+Q6 <- Q4
+Q6 <- Q5
+
+Q7 <- c(1975, 1952, 1975)
+
+Q8 <- data.frame(
+  name <- Q5,
+  death_year <- Q7
+)
+
+Q9 <- mean(Q7)
+
+
+Q10 <- Q8
+Q10[Q10 == "death_year" ] <- factor(Q7)
