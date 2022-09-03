2b5fdade272fc4ed213a6d439584a11c272a53a3
Jaime Rodriguez <jaime@Jaimes-MacBook-Pro.local>
2022-09-02 20:55:37 -0400

    Edit Intro Lab 2

--- Module 1/Intro/Intro Lab 2.R
+++ Module 1/Intro/Intro Lab 2.R
@@ -1,17 +1,17 @@
 library(airports)
 
-Q1 <- rows <- NROW(usairports)
+Q1 <- nrow(usairports)
 
-Q2 <- usairports
+Q2 <- class(usairports)
 
-Q3 <- Q2
+Q3 <- class(Q2)
 
 Q4 <- c(1, 2, 3)
 
 Q5 <- c("Larry Fine", "Curly Howard", "Moe Howard")
 
 Q6 <- Q4
-Q6 <- Q5
+names(Q6) <- Q5
 
 Q7 <- c(1975, 1952, 1975)
 
